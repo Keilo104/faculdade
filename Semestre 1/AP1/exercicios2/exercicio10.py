e503f019e80f4c0054bb22e36863e03993ee57d9
Keilo <keilogamimg@gmail.com>
2020-02-20 16:19:30 -0300

    altas mudança slc

--- Semestre 1/AP1/exercicios2/exercicio10.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Digite um número: "))
-
-mod = num % 3
-
-if mod == 0:
-    print("O número {} é divisível por 3".format(num))
-else:
-    print("O número {} não é divisível por 3".format(num))
--- Semestre 1/AP1/exercicios2/exercicio16.py
+++
@@ -1,6 +0,0 @@
-dia = int(input("Digite o dia atual: "))
-mes = int(input("Digite o mês atual: ")) - 1
-
-diasRest = 360 - (dia + (mes * 30))
-
-print("Faltam {} dias para o ano acabar.".format(diasRest))
